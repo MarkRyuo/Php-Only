1df92d3a73463e2b4aedddb8fc8291db2eeb734d
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-05 10:58:12 +0800

    update in php

---
+++ Firstphpcodes/php/Variable/variable-2.php
@@ -0,0 +1,21 @@
+<?php 
+
+// Variable
+
+$Name = 10;
+$Name = "This is 10" ;
+// echo $Name 
+
+// php is case sensitive 
+
+$Example = "This is Uppercase";
+$example = "This lowercase";
+
+echo $Example 
+// echo $example
+
+
+
+
+
+?>
