35272be3e78fbe2d2a10d18fff555fddb9b889c5
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-05 11:02:53 +0800

    update in php

--- Firstphpcodes/php/Variable/variable-2.php
+++ Firstphpcodes/php/Variable/variable-2.php
@@ -11,8 +11,16 @@ $Name = "This is 10" ;
 $Example = "This is Uppercase";
 $example = "This lowercase";
 
-echo $Example 
+// echo $Example 
 // echo $example
+
+// This is correct naming variable 
+
+$Jhonmark = "Jan 19 2003" ;
+$jhonmark = "01/19/03"; 
+$_Jhonmark = "January, 19 2003";
+
+echo $_Jhonmark 
 
 
 
