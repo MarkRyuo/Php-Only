7647d74f8c87eb8b3e8b2f038b96d8b4ff55765e
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-05 10:51:43 +0800

    update in line 13

---
+++ Firstphpcodes/php/variable.php
@@ -0,0 +1,16 @@
+<?php 
+
+// variable start in dollar sign ($)
+// $example = 10;
+// echo $example
+
+// php is case sensitive 
+// $hatboy = 20;
+// $namE = "Wow";
+// echo $namE 
+
+
+$kalbo = "weoolll";
+echo $kalbo
+
+?>
