9da01ba303337695c3893400519fc7525537ead5
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-21 13:04:45 +0800

    update here

--- php-2/variable-1.php
+++ php-2/variable-1.php
@@ -9,4 +9,11 @@ $name = "Jhon Mark"; //This is Example of String
 
 $age = 20; // Example of integer 
 
+//This is Float 
+
+$currency = 40.5; //Example of float like money in US ($40.5)
+
+// This is Boolean 
+
+$online = true; // Example of Boolean is "Is this Online? true or false : ans true
 ?>
