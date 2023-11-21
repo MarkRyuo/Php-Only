762ffbdab6caa632556757c85db295a6eb407c6b
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-21 12:19:14 +0800

    update here

--- php-2/index.php
+++ php-2/index.php
@@ -2,10 +2,14 @@
 
  echo "Hi <br>";
  echo "Im Jhon Mark";
-
+// <br> means break line
 //  This is comment in short 
 /* And this is 
 multiline comment
 for long */
+
+echo "20 years old <br>";
+echo "Currently Learning (Php) <br>" ;
+echo "or Hypertext preprocessor";
 
 ?>
