b9162d03279c8126540de7efa7e6af429d862709
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-21 13:14:10 +0800

    update here

--- php-2/variable-1.php
+++ php-2/variable-1.php
@@ -17,7 +17,9 @@ $currency = 40.5; //Example of float like money in US ($40.5)
 
 $online = true; // Example of Boolean is "Is this Online? true or false : ans true
 
-/* if you use currency in text use the escape sequence (/) like this /${currency}; 
+/* if you use currency in text use the escape sequence (/) like this $/{currency}; 
 output : $40.5 */
 
+
+echo"My name is {$name} I'm {$age} years old <br> my Money is  $/{$currency} and im {$online} now ";
 ?>
