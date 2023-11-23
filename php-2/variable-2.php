30e4ab3b619210b78ef622db99b10379bc59d4fb
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-23 08:39:14 +0800

    update here

--- php-2/variable-2.php
+++ php-2/variable-2.php
@@ -1,21 +1,30 @@
 <?php 
 
+/* Exercise 1: Basic Variables
+Create a variable called $name and assign your name to it.
+Create a variable called $age and assign your age to it.
+Create a variable called $city and assign the name of your city to it.
+Print out these variables in a sentence like "My name is [name], I am [age] years old, and I live in [city]." */
+
 
 
 //variable 
 
 //Todo String
 
+$name = "Mark Ryuo";
+$city = "Lipa City";
 
 //Todo Integer 
 
+$age = 20 ;
 
 //Todo Float 
 
 
 //Todo Boolean 
 
-
+echo "My name is "
 
 
 
