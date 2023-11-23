89811c6868906bbe9dc6c5ba7e2ebb16e86cd4b7
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-23 22:17:33 +0800

    update here

--- Arithmetic/Arithmetic-1.php
+++ Arithmetic/Arithmetic-1.php
@@ -8,10 +8,15 @@
 
 //Todo First we use 
 
-$budget = 10; // This is Normal Int
-$sale = 3 ; // This is Float
+// $budget = 10; // This is Normal Int
+// $sale = 3.5 ; // This is Float
+
+$budget = 14; // This is Normal Int
+$sale = 3 ; // 14 * 14 * 14 
 
 $total = $budget ** $sale;
 echo $total;
+
+
 
 ?>
