51875565f7e576bea52a0f9241d880a541d57147
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-23 08:47:31 +0800

    update here

---
+++ php-2/Exercise-2.php
@@ -0,0 +1,20 @@
+<?php 
+
+/* Exercise 2: Variable Types
+Create a variable called $num1 and assign an integer value to it.
+Create a variable called $num2 and assign a floating-point value to it.
+Create a variable called $isStudent and assign a boolean value (true or false) to it.
+Print out the types of these variables.
+*/
+
+$num1 = 20;
+$num2 = 20.5;
+
+$isStudent = true;
+
+// echo $num1
+echo $num2
+
+
+
+?>
