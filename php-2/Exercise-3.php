30ddf86898964fd0076fc948b3053cdd275312ac
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-23 10:39:02 +0800

    update here

--- php-2/Exercise-3.php
+++ php-2/Exercise-3.php
@@ -3,7 +3,7 @@
 /*
 Exercise 3: Variable Manipulation
 Create two variables, $num3 and $num4, and assign them integer values.
-Calculate the sum, difference, product, and quotient of these two numbers and store the results in separate variables.
+Calculate the sum (+), difference (-), product(*), and quotient (/) of these two numbers and store the results in separate variables.
 Print out the results in a readable format, e.g., "The sum is [sum], the difference is [difference], and so on."
 
 */
