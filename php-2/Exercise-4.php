3332f6b63959fca266579bb9b0a897c9a95d7e18
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-23 14:49:25 +0800

    update here

---
+++ php-2/Exercise-4.php
@@ -0,0 +1,34 @@
+<?php 
+
+
+/* Todo 
+Exercise 4: Concatenation
+Create two variables, $firstName and $lastName, and assign your first and last name to them.
+Concatenate these variables to create a full name and store it in a new variable called $fullName.
+Print out the full name.
+*/
+
+//Todo string 
+// $example = "This is String";
+
+$firstName = "Mark Ryuo";
+$lastName = "Malupa";
+
+$fullName = $firstName + $lastName;
+
+echo $fullName
+
+
+
+
+//Todo Integer 
+//$example = 10 ;
+
+//Todo float 
+//$example = 21.1;
+
+//Todo Boolean 
+//$example = true;
+
+
+?>
