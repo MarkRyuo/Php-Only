4b240e8749c8d82c35e0b5290118d94d72551908
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-28 21:30:37 +0800

    update here

--- Arithmetic/Arithmetic-2.php
+++ Arithmetic/Arithmetic-2.php
@@ -9,7 +9,18 @@ $numadd2 = 700;
 
 $add = $numadd1 + $numadd2;
 
-echo $add; 
+// echo $add; 
+
+//Todo Subtraction
+
+$numsub1 = 450;
+$numsub2 = 700;
+
+$sub $numsub1 - $numsub2;
+
+echo $sub ;
+
+
 
 
 
