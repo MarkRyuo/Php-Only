be3cae684295dd6695b57529914b0832adcb568b
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-12 22:19:30 +0800

    update here

--- Arithmetic/Arithmetic-7.php
+++ Arithmetic/Arithmetic-7.php
@@ -6,6 +6,6 @@ $Cellphone = 4 ;
 $Cellphone ++;
 $nphone = "Iphone X";
 
-echo "I have 4 <br>", $nphone, ",", "<br> and 1 missing <br>", 
+echo "I have 4 <br>", $nphone, ",", "<br> and 1 missing <br>", "the total of my iphone now is, <br>", $Cellphone ; 
 
 ?>
