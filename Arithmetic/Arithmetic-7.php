0ec8ae2c16c7c494fc1bd5a06b474336c8caa5bd
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-12 22:40:22 +0800

    update here

--- Arithmetic/Arithmetic-7.php
+++ Arithmetic/Arithmetic-7.php
@@ -9,7 +9,7 @@ $nphone = "Iphone X";
 echo "I have 4 ", $nphone, " ", "and 1 missing", " ", "the total of my iphone now is"," ", $Cellphone, "<br>" ; 
 
 // Todo (+=) & (-=)
-$sample = 10; // Ihave 10 box i need to add 5 box
+$sample = 20; // Ihave 20 box i need to add 5 box
 $sample +=5; // Use this (+=) to add 
 
 echo $sample, "<br>";
