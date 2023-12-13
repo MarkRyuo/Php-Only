4f4dacb8c5268ce48ce3790590af17f3201c2290
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-13 11:13:46 +0800

    update here

--- Arithmetic/Arithmetic-8.php
+++ Arithmetic/Arithmetic-8.php
@@ -4,13 +4,15 @@
 
 
 $pencil = 4 ;
-$pencil +=5 ;
+// $pencil +=5 ; 
 
 // Ilan na ang pencil ko ?
 
 echo $pencil ;
 
 //binigay ko sa aking kapatid ang 3 pencil kaya ilan nalang ang pencil ko
+
+$pencill -=3;
 
 
 ?>
