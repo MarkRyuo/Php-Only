9c47dfbe9e27806ccaeb93da96bd65ecbeb2aa12
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-13 19:52:39 +0800

    udpate here

--- Arithmetic/Arithmetic-9.php
+++ Arithmetic/Arithmetic-9.php
@@ -8,7 +8,10 @@
 
 $total = 2 + 3 - 5 * 4 / 5 ** 6 ;
 // $total = 4.99872 ;
-echo $total ;
+echo $total, "<br>" ;
 
 $box = 2 + 4 - 5 * 7 / 10 ** 4 ;
+$box = 2 + 4 - 5 * 7 / 10 ** 4 ;
+
+echo $box ;
 ?>
