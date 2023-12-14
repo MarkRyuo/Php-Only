086f016d81ee678ab1a6d64b077cd8ab46e4515c
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-14 22:41:57 +0800

    update here

--- Arithmetic/Arithmetic-10.php
+++ Arithmetic/Arithmetic-10.php
@@ -6,7 +6,7 @@ $fname = "Jhon Mark" ;
 $lname = "Malupa" ;  // String 
 $age = "20" ; //Integer 
 
-echo "My name is", " ", $fname, " ", $lname, " ", "I'm", " ", $age ;
+echo "My name is", " ", $fname, " ", $lname, " ", "I'm", " ", $age, "<br>" ;
 
 //Todo + - * / ** % 
 
