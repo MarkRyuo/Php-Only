558f6c5e90d6a9e6687312b46b95cab2b7e42b1d
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-17 14:55:40 +0800

    updat here

--- POST&GET/tezt.php
+++ POST&GET/tezt.php
@@ -10,4 +10,14 @@ if ($number1 >= $number2) {
 }
 
 
+$number3 = 7;
+$number4 = 12;
+
+if ($number3 <= $number4) {
+    echo "Ang $number3 ay mas maliit o katumbas kaysa sa $number4.";
+} else {
+    echo "Ang $number3 ay mas malaki kaysa sa $number4.";
+}
+
+
 ?>
