ed38d677065e6755aa37d7da36aaeb4878814251
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-17 20:20:01 +0800

    update here

--- If&else/if&else.php
+++ If&else/if&else.php
@@ -6,6 +6,8 @@ $age +=10;
 //I add increment to the age
 
 //Todo kung ang age ay mas mataas or equal sa 20 ang output dapat ay (Your too old buddy)
+
+//kung ang age is 10 and the add is 20 
  if ($age >= 20) {
   echo "Your too old buddy" ;
  } 
