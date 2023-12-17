f31584d65c03ae2aa9ac231c8d55534faf5204b4
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-17 20:03:57 +0800

    update here

--- If&else/if&else.php
+++ If&else/if&else.php
@@ -5,11 +5,11 @@ $age = 10 ;
 
 //Todo kung ang age ay mas mataas or equal sa 20 ang output dapat ay (Your too old buddy)
  if ($age >= 20) {
-  echo "This your to old buddy" ;
+  echo "Your too old buddy" ;
  } 
 
  else 
-  echo "your to young buddy"
+  echo "Your too young buddy"
 
 
 ?>
