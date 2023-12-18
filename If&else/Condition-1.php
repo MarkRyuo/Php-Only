029cb0606241356d30531925437a8d2c93a30736
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-18 16:56:18 +0800

    update herE

--- If&else/Condition-1.php
+++ If&else/Condition-1.php
@@ -1,14 +1,19 @@
 <?php 
 
-$x = 40 ;
+$x = 2 ;
 $y = 40 ;
 
+//Todo if ang x is equal sa y (ex. x = 70 y = 70) this is equal 
 if ($x == $y) {
   echo "x and y is equal number" ;
 }
 
 elseif ($x >= $y) {
   echo "x is greater than the y" ;
+}
+
+elseif ($x <= 5) {
+  echo "x is not take your low number" ;
 }
 
 else {
