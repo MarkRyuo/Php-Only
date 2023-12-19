49b62adc964d8429f452fa7b49a0ec1c5b94f1d7
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-19 11:21:41 +0800

    update here

--- If&else/Condition-3.php
+++ If&else/Condition-3.php
@@ -1,16 +1,9 @@
 <?php 
 
-$adult = null ;
+$adult = true ;
 
 if ($adult >= true) {
   echo "Its true your adult bro" ;
-}
-// 
-elseif ($adult <= null)  {
-  echo "Bro are you AI?" ;
-}
-elseif ($adult >= 1000) {
-  echo "bro are you alien ?" ;
 }
 else {
   echo "Bro your not adult" ;
